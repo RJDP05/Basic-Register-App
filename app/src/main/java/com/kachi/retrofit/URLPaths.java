04268827c00f939fefe07c2f42dca03ecb05735d
Rjdp <rajdeepkhamrai10@gmail.com>
2024-11-12 22:01:04 +0530

    Url Chnage

--- app/src/main/java/com/kachi/retrofit/URLPaths.java
+++ app/src/main/java/com/kachi/retrofit/URLPaths.java
@@ -1,7 +1,8 @@
 package com.kachi.retrofit;
 
 public class URLPaths {
-    public static String MAIN_URL = "http://192.168.216.97/myAPI.php/";
+    public static String MAIN_URL = "http://192.168.151.97/myAPI.php/";
 //    public static String LOGIN_URL = MAIN_URL+"login";
 //    public static String REGISTER_URL = MAIN_URL+"register";
+
 }
